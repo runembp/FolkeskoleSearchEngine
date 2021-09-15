942df9ef28828ee48bacc075438aab23a8652fb4
Rune Petersen <runembpetersen@gmail.com>
2021-09-15 18:17:20 +0200

    Initial commit

---
+++ Data/School.cs
@@ -0,0 +1,12 @@
+﻿namespace FolkeskoleSearchEngine.Data
+{
+    public class School
+    {
+        public string Id { get; set; }
+        public string Navn { get; set; }
+        public string Type { get; set; }
+        public string Address { get; set; }
+        public string Telephone { get; set; }
+        public string WebAddress { get; set; }
+    }
+}
---
+++ Data/SchoolService.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FolkeskoleSearchEngine.Data
+{
+    public class SchoolService
+    {
+        private List<School> _schoolList = new();
+        
+        public Task<List<School>> ListFromCsv()
+        {
+            _schoolList = new List<School>();
+            var csvData = File.ReadAllLines("folkeskoler.csv");
+
+            for(var i = 1; i < csvData.Length; i++)
+            {
+                if(!csvData[i].Contains(";"))
+                    continue;
+                
+                var splitData = csvData[i].Split(";");
+                _schoolList.Add(new School()
+                {
+                    Id = splitData[1],
+                    Navn = splitData[2],
+                    Address = $"{splitData[4]}, {splitData[5]} {splitData[6]}",
+                    Telephone = splitData[7],
+                    WebAddress = splitData[9],
+                    Type = splitData[11]
+                });
+            }
+
+            return Task.FromResult(_schoolList);
+        }
+
+        public Task<List<School>> ListFromCsvWithNameFiltered(string userInput = "") 
+        {
+            var filteredSchoolList = _schoolList.Where(x => x.Navn.Contains(userInput)).ToList();
+            
+            return Task.FromResult(filteredSchoolList);
+        }
+    }
+}
